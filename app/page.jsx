c8e9e5d577c639c81aa559c03ea8b4c7ffc322f8
2S-Global <2sglobaltechnologies@gmail.com>
2025-03-31 15:40:54 +0530

    testing

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Home from "@/components/home-1";
 import LogIn from "@/components/pages-menu/login";
 
 export const metadata = {
-  title: "E²-Score",
+  title: "E²-Score2",
   description: "E²-Score Job portal",
 };
 
