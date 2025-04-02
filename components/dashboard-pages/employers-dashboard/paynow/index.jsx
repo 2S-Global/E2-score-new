6d87e31cf036296d8650dcf4e5a2f9a4f0dae4c7
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-02 12:27:24 +0530

    layout fix

--- components/dashboard-pages/employers-dashboard/paynow/index.jsx
+++ components/dashboard-pages/employers-dashboard/paynow/index.jsx
@@ -30,12 +30,7 @@ const index = () => {
             {/* <!-- Dashboard --> */}
             <section className="user-dashboard">
                 <div className="dashboard-outer">
-                    <BreadCrumb title="Pay Now!" />
-                    {/* breadCrumb */}
-
-                    <MenuToggler />
-                    {/* Collapsible sidebar button */}
-
+                   
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="ls-widget">
