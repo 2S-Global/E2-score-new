9854abc1ebd59c180e7954ddccfff7a1ae0aa780
2S-Global <2sglobaltechnologies@gmail.com>
2025-05-27 11:55:43 +0530

    update

--- app/institute-dashboard/dashboard/page.jsx
+++ app/institute-dashboard/dashboard/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import dynamic from "next/dynamic";
-import DashboadHome from "../../../components/dashboard-pages/Institute-dashboard/dashboard";
+// import DashboadHome from "../../../components/dashboard-pages/Institute-dashboard/dashboard";
+import DashboadHome from "../../../components/dashboard-pages/institute-dashboard/dashboard";
+
 // import DashboadHome from "../../../"; 
 
 const index = () => {
