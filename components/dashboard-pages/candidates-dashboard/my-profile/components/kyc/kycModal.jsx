b3961ae707810686c8c04209af899431fdb7574a
2S-Global <2sglobaltechnologies@gmail.com>
2025-09-15 14:43:10 +0530

    updated modal

--- components/dashboard-pages/candidates-dashboard/my-profile/components/kyc/kycModal.jsx
+++ components/dashboard-pages/candidates-dashboard/my-profile/components/kyc/kycModal.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Sparkles } from "lucide-react"; // AI suggestion icon
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-import KycBox from "./Kycbox";
+import KycBox from "./Kycboxnew";
 
-const KycModal = ({ show, onClose, setError, setSuccess }) => {
+const KycModal = ({
+  show,
+  onClose,
+  setError,
+  setSuccess,
+  setMessageId,
+  setErrorId,
+  setReload,
+  setSectionloading,
+  focusSection,
+}) => {
   if (!show) return null;
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [saving, setSaving] = useState(false);
+
   return (
     <>
       <div
@@ -40,6 +53,49 @@ const KycModal = ({ show, onClose, setError, setSuccess }) => {
               >
                 Cancel
               </button>
+              <style jsx>{`
+                .tooltip-wrapper {
+                  position: relative;
+                  display: inline-block;
+                }
+
+                .tooltip-wrapper .custom-tooltip {
+                  visibility: hidden;
+                  background-color: white;
+                  color: red;
+                  font-weight: bold;
+                  text-align: center;
+                  border: 1px solid red;
+                  border-radius: 4px;
+                  padding: 5px 10px;
+                  position: absolute;
+                  bottom: 100%;
+                  left: 0;
+                  margin-bottom: 6px;
+                  z-index: 1;
+                  white-space: nowrap;
+                }
+
+                .tooltip-wrapper:hover .custom-tooltip {
+                  visibility: visible;
+                }
+              `}</style>
+
+              <div className="tooltip-wrapper">
+                {!isFormValid && (
+                  <div className="custom-tooltip">
+                    Please fill all required fields
+                  </div>
+                )}
+
+                <button
+                  className="btn btn-primary"
+                  // onClick={handleSave}
+                  disabled={!isFormValid || saving}
+                >
+                  <>{saving ? "Saving..." : "Save"}</>
+                </button>
+              </div>
             </div>
           </div>
         </div>
