8bac1e5db755a8f1ff3f0094bc878ba7983ddede
2S-Global <2sglobaltechnologies@gmail.com>
2025-11-04 10:52:44 +0530

    Fix logo image component in FormContent2: update src path and adjust dimensions

--- components/common/form/login/FormContent2.jsx
+++ components/common/form/login/FormContent2.jsx
@@ -83,11 +83,12 @@ const FormContent2 = () => {
   return (
     <div className="form-inner p-4">
       <div className="mb-3 d-flex justify-content-center pb-4 pt-4">
-        <img
+        <Image
           alt="brand"
-          src="/images/logo3.png"
-          width={214}
-          height={70}
+          src="/images/Logo3.png"
+          width={154}
+          height={60}
+          style={{ height: "60px" }}
           priority
         />
       </div>
