e441d02e747c7350572e70ca0740d6b189f5a0b3
2S-Global <2sglobaltechnologies@gmail.com>
2025-11-11 12:18:33 +0530

    . condition

--- components/candidates-single-pages/new-components/Certificationblock.jsx
+++ components/candidates-single-pages/new-components/Certificationblock.jsx
@@ -53,20 +53,24 @@ const CertificationBlock = ({ data = [] }) => {
                   </a>
                 )}
 
-                {item.validityFrommonth && item.validityFromyear && (
-                  <span>
-                    Valid from:{" "}
-                    {`${monthNames[item.validityFrommonth - 1]} ${item.validityFromyear}.`}
-                  </span>
-                )}
-
-                <span className="mx-2">
-                  {item.doesNotExpire
-                    ? "Does not expire."
-                    : item.validityToMonth && item.validityToyear
-                      ? `Valid till: ${monthNames[item.validityToMonth - 1]} ${item.validityToyear}.`
-                      : ""}
+                <span>
+                  Valid from: {monthNames[item.validityFrommonth - 1]}{" "}
+                  {item.validityFromyear}
+                  {item.validityFromyear ? "." : ""}
                 </span>
+
+                {item.doesNotExpire ? (
+                  <>
+                    <span className="mx-2">Does not expire.</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="mx-2">
+                      Valid till: {monthNames[item.validityToMonth - 1]}{" "}
+                      {item.validityToyear}.
+                    </span>
+                  </>
+                )}
               </div>
             ))
           ) : (
