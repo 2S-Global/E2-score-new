c931b18073b5242661ce4bbabca724b8f757cd7a
2S-Global <2sglobaltechnologies@gmail.com>
2025-12-15 12:53:20 +0530

    vps sync

--- components/newhome/components/HeaderNavContent.jsx
+++ components/newhome/components/HeaderNavContent.jsx
@@ -42,92 +42,36 @@ const HeaderNavContent = () => {
           >
             <Link href="#about">About</Link>
           </li>
-
-          {/* End homepage menu items */}
-
-          {localStorage.getItem("employer_token") ? null : (
-            <li
-              className={`${
-                isActiveParent(findJobItems, usePathname()) ? "current" : ""
-              } dropdown has-mega-menu`}
-              id="has-mega-menu"
-            >
-              <Link href="/job-list">Jobs</Link>
-            </li>
-          )}
-          {/* End findjobs menu items */}
-
-          {localStorage.getItem("employer_token") ? null : (
-            <li
-              className={`${
-                isActiveParent(employerItems, usePathname()) ||
-                usePathname()?.split("/")[1] === "employers-dashboard"
-                  ? "current"
-                  : ""
-              } dropdown`}
-            >
-              <Link href="/employers-list">Employers</Link>
-            </li>
-          )}
-          {/* End Employers menu items */}
-          {localStorage.getItem("candidate_token") ? null : (
-            <li
-              className={`${
-                isActiveParent(candidateItems, usePathname()) ||
-                usePathname()?.split("/")[1] === "candidates-dashboard"
-                  ? "current"
-                  : ""
-                    ? "current"
-                    : ""
-              } dropdown`}
-            >
-              <Link href="/candidates-list">
-                <span>Candidates</span>
-              </Link>
-            </li>
-          )}
-          {/* End Candidates menu items */}
-
           <li
             className={`${
-              isActiveParentChaild(blogItems, usePathname()) ? "current" : ""
+              isActiveParent(homeItems, usePathname()) ? "current" : ""
             } dropdown`}
           >
-            <Link href="/blog-list">
-              <span>Blog</span>
-            </Link>
+            <Link href="#service">Services</Link>
           </li>
-          {/* End Blog menu items */}
-
           <li
             className={`${
-              isActiveParent(about, usePathname()) ||
-              usePathname()?.split("/")[1] === "about"
-                ? "current"
-                : ""
-                  ? "current"
-                  : ""
+              isActiveParent(homeItems, usePathname()) ? "current" : ""
             } dropdown`}
           >
-            <Link href="/about">
-              <span>About</span>
-            </Link>
+            <Link href="#clients">Clients</Link>
           </li>
-          {/* End About menu items */}
-
           <li
             className={`${
-              isActiveParent(contact, usePathname()) ||
-              usePathname()?.split("/")[1] === "contact"
-                ? "current"
-                : ""
-                  ? "current"
-                  : ""
+              isActiveParent(homeItems, usePathname()) ? "current" : ""
             } dropdown`}
           >
-            <Link href="/contact">
-              <span>Contact</span>
-            </Link>
+            <Link href="#testimonials">Testimonials</Link>
+          </li>
+          <li className="dropdown">
+            <Link href="#app-section">App</Link>
+          </li>
+          <li
+            className={`${
+              isActiveParent(homeItems, usePathname()) ? "current" : ""
+            } dropdown`}
+          >
+            <Link href="#contact">Contact</Link>
           </li>
 
           {/* End Pages menu items */}
